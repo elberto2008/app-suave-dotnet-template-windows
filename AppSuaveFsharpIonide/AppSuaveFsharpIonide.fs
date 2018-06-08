bf9d8b9fda86effce3512ab97cf69f40dcb8a86b
el <felicien.fotiomanfo@gmaill.com>
2018-06-08 04:12:10 +0100

    Squelleton working

--- AppSuaveFsharpIonide/AppSuaveFsharpIonide.fs
+++ AppSuaveFsharpIonide/AppSuaveFsharpIonide.fs
@@ -2,28 +2,15 @@ open Suave                 // always open suave
 open Suave.Successful      // for OK-result
 open Suave.Operators
 open Suave.Filters
-open OrderTaking.Common
-open OrderTaking.PlaceOrder
-open OrderTaking.PlaceOrder.Api
-
-
-let requestBody:JsonString =  "request body"
-
-let request =  {
-    Action = "Place Order"
-    Uri = "api/v1/place-order"
-    Body = requestBody
-    }
+open OrderContext.RestApi
 
 
 
-let app = 
-    choose [ GET >=> choose
-                [ path "/" >=> OK "This is my Home Page"
-                  path "/hello" >=> OK "This is my GET Hello Page" ]
-             POST >=> choose
-                [ path "/hello" >=> OK "This is my POST Hello Page"
-                  path "/place-order" >=> OK "This is my place-order" ]
-    ]
 
-startWebServer defaultConfig app
+
+let app = peopleWebPart
+    
+
+let myServerConfig = { defaultConfig with bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" 8085 ]}
+
+startWebServer myServerConfig app
---
+++ AppSuaveFsharpIonide/OrderContext.ResfulApi.fs
@@ -0,0 +1,80 @@
+module OrderContext.RestApi
+
+open OrderTaking.PlaceOrder.Api
+
+open Newtonsoft.Json
+open Newtonsoft.Json.Serialization
+open Suave
+open Suave.Successful
+open Suave.Operators
+open Suave.Filters
+open System.Collections.Generic
+open OrderTaking.PlaceOrder.Api
+open Suave.Http
+open Suave
+
+
+[<AutoOpen>]
+
+
+module RestFul = 
+
+    type RestResource<'a> = {
+        GetAll: unit -> 'a seq
+        Create: 'a -> 'a 
+    }
+
+    type Person = {
+        Id : int
+        Name : string
+        Age : int
+        Email : string
+    }
+
+let person1 = {Id=1; Name="Felix M. Fotio"; Age=33; Email="felix@gmail.com"}
+let person2 = {Id=2; Name="Megan A. Hess"; Age=28; Email="felix@gmail.com"}
+
+module Db = 
+    let private peopleStorage = new Dictionary<int, Person>()
+    peopleStorage.Add(person1.Id, person1)
+    peopleStorage.Add(person2.Id, person2)
+    let getPeople () = 
+        peopleStorage.Values :> seq<Person>
+    let createPerson person = 
+        let id = peopleStorage.Values.Count + 1
+        let newPerson = {person with Id = id}
+        peopleStorage.Add(id, person)
+        newPerson
+
+
+//Helper Functions
+//JSON GLOBAL HELPER FUNTION to transform any generic type 'a into a type HttpContext->Async<HttpContext>
+let JSON v = 
+    let settings = new JsonSerializerSettings()
+    settings.ContractResolver <- new CamelCasePropertyNamesContractResolver()
+    JsonConvert.SerializeObject(v, settings)
+    |> OK
+    >=> Writers.setMimeType "application/json; charset=utf-8"
+
+let fromJson<'a> json = 
+    JsonConvert.DeserializeObject(json, typeof<'a>) :?> 'a
+    
+let getResourceFromReq<'a> (req: HttpRequest) = 
+    let getString rawForm = 
+        System.Text.Encoding.UTF8.GetString(rawForm)
+    req.rawForm |> getString |> fromJson<'a>
+   
+
+let rest restResourceName resource = 
+    let resourcePath = "/" + restResourceName
+    let getAll = warbler(fun _ ->resource.GetAll () |> JSON)
+    path resourcePath >=> choose [
+        Filters.GET >=> getAll
+        Filters.POST >=> request (getResourceFromReq >> resource.Create >> JSON)
+    ]
+
+
+let peopleWebPart = rest "place-order" {
+    GetAll = Db.getPeople
+    Create = Db.createPerson    
+}
